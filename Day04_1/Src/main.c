30e85596cba6b30d316b4dae1fe32fd5d78444e0
Shubham Bhadke <shubhambhadke148@gmail.com>
2024-03-29 05:14:51 +0530

    Day04 Assignment Q.1

---
+++ Day04_1/Src/main.c
@@ -0,0 +1,39 @@
+// 1. Scroll LCD display to left (repeatedly after 1 second).
+// Stop scrolling after scrolling 16 times.
+
+#include <stdint.h>
+#include <stdio.h>
+#include "stm32f4xx.h"
+#include "system_stm32f4xx.h"
+
+#include "lcd.h"
+
+#if !defined(__SOFT_FP__) && defined(__ARM_FP)
+  #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
+#endif
+
+int main(void)
+{
+	SystemInit();
+	LcdInit();
+	//LcdPuts(LCD_LINE1, "DESD @ SUNBEAM");
+	while(1)
+	{
+		LcdPuts(LCD_LINE2, "krishna          ");
+		LcdPuts(LCD_LINE2, "rishna           k");
+		LcdPuts(LCD_LINE2, "ishna           kr");
+		LcdPuts(LCD_LINE2, "shna           kri");
+		LcdPuts(LCD_LINE2, "hna           kris");
+		LcdPuts(LCD_LINE2, "na           krish");
+		LcdPuts(LCD_LINE2, "a           krishn");
+		LcdPuts(LCD_LINE2, "           krishna");
+    }
+	return 0;
+}
+
+
+
+
+
+
+
